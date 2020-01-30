fa3c3f25dfd2402bc5fed2be6b48098a528f092c
Navjot Singh <Nav_Singh@manulife.com>
2020-01-29 23:34:43 -0500

    trip calculation done

--- src/CoffeMachine.kt
+++ src/CoffeMachine.kt
@@ -18,4 +18,30 @@ fun main() {
     println(num3)
     val num4 = scanner.next()
     println(num4)
+    read5words()
+    flightTripCalculate()
+}
+
+fun read5words() {
+    val scanner = Scanner(System.`in`)
+    //var word = ""
+    //for (i in 1..5){
+    val word1 = scanner.nextLine()
+    val word2 = scanner.nextLine()
+    println(word1)
+    println(word2)
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    println(num1)
+    println(num2)
+    // }
+}
+
+fun flightTripCalculate(){
+    val scanner = Scanner(System.`in`)
+    val days = scanner.next().toInt()
+    val flighCost = scanner.next().toInt()
+    val foodCostPerDay = scanner.next().toInt()
+    val oneNightCost = scanner.next().toInt()
+    println(((days * foodCostPerDay) + ((days -1) * oneNightCost)) + (flighCost * 2))
 }
