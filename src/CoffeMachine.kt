eeb6b2404cc4ae890b62eec1ae0c24542ae16ce0
Navjot Singh <Nav_Singh@manulife.com>
2020-01-29 23:00:17 -0500

    scanner next func added

--- src/CoffeMachine.kt
+++ src/CoffeMachine.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 fun main() {
     println("Starting to make a coffee")
     println("Grinding coffee beans")
@@ -6,4 +8,14 @@ fun main() {
     println("Pouring coffee into the cup")
     println("Pouring some milk into the cup")
     println("Coffee is ready!")
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val num1 = scanner.next()
+    println(num1)
+    val num2 = scanner.next()
+    println(num2)
+    val num3 = scanner.next()
+    println(num3)
+    val num4 = scanner.next()
+    println(num4)
 }
